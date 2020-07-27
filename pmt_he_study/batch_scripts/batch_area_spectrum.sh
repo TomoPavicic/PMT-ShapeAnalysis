d9795a90632f0a2c63a83fcca1404d7b28eac752
William Quinn <william.quinn.14@ucl.ac.uk>
2020-07-27 15:36:46 +0100

    Couple of changes

--- pmt_he_study/batch_scripts/batch_area_spectrum.sh
+++ pmt_he_study/batch_scripts/batch_area_spectrum.sh
@@ -38,15 +38,15 @@ file_filenames="filenames_$voltage.ascii"
 
 root_dir="/unix/nemo4/PMT_He_Study_nemo4"
 script_log_dir=batch_log_$tag
-mkdir $root_dir/batch_files/$script_log_dir
-mkdir $root_dir/batch_files/$script_log_dir/scripts
-mkdir $root_dir/batch_files/$script_log_dir/logs
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs
 
-ls $root_dir/data/raw_xml_files/*/*$voltage* /unix/nemo3/PMT_He_Study_nemo3/data/raw_xml_files/*/*$voltage* > $root_dir/batch_files/$script_log_dir/$file_filenames
-data_file_list=`cat $root_dir/batch_files/$script_log_dir/$file_filenames`
+ls $root_dir/data/raw_xml_files/*/*$voltage* /unix/nemo3/PMT_He_Study_nemo3/data/raw_xml_files/*/*$voltage* > $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/$file_filenames
+data_file_list=`cat $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/$file_filenames`
 #
 # ROOT macro to use:
-root_macro="$root_dir/supernemopmts/pmt_charge_spectrum.cpp"
+root_macro="$root_dir/PMT-ShapeAnalysis/pmt_he_study/pmt_charge_spectrum.cpp"
 
 echo $voltage
 echo ""
@@ -76,16 +76,16 @@ do
     macro_plus_arguments="\"$root_macro(\\\"$file\\\",\\\"$date\\\",$voltage)\""
     echo $macro_plus_arguments
     #
-    interactive_command="root.exe -q -b $macro_plus_arguments >& $root_dir/batch_files/$script_log_dir/logs/batch_file_$ifile.log"
+    interactive_command="root.exe -q -b $macro_plus_arguments >& $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs/batch_file_$ifile.log"
     echo $interactive_command
     # $interactive_command
     #
     # Prepare batch script in a temporary directory (not sure if this is robust)
-    touch $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    chmod a+rwx $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    echo "echo 'Running batch_file_$ifile.sh ...' >> $root_dir/batch_files/$script_log_dir/logs/batch_file_$ifile.log" > $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    echo $interactive_command >> $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    batch_command="qsub -q $queue $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh"
+    touch $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    chmod a+rwx $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    echo "echo 'Running batch_file_$ifile.sh ...' >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs/batch_file_$ifile.log" > $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    echo $interactive_command >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    batch_command="qsub -q $queue $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh"
     echo $batch_command
     # For testing (without submitting to the batch farm), comment out the next line:
     $batch_command
--- pmt_he_study/batch_scripts/batch_xml_reader.sh
+++ pmt_he_study/batch_scripts/batch_xml_reader.sh
@@ -15,18 +15,18 @@ do
 done
 root_dir="/unix/nemo4/PMT_He_Study_nemo4"
 script_log_dir=batch_log_$tag
-mkdir $root_dir/batch_files/$script_log_dir
-mkdir $root_dir/batch_files/$script_log_dir/scripts
-mkdir $root_dir/batch_files/$script_log_dir/logs
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts
+mkdir $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs
 #
 #
 #
 # Files found  automatically
-ls $root_dir/data/raw_xml_files/*/*.xml /unix/nemo3/PMT_He_Study_nemo3/data/raw_xml_files/*/*.xml > $root_dir/batch_files/$script_log_dir/filenames.ascii
-data_file_list=`cat $root_dir/batch_files/$script_log_dir/filenames.ascii`
+ls $root_dir/data/raw_xml_files/*/*.xml /unix/nemo3/PMT_He_Study_nemo3/data/raw_xml_files/*/*.xml > $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/filenames.ascii
+data_file_list=`cat $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/filenames.ascii`
 #
 #
-config_file="$root_dir/PMT-ShapeAnalysis/config_files/pmt_permeation_config_file.txt"
+config_file="$root_dir/PMT-ShapeAnalysis/config_files/pmt_permeation_config.conf"
 #
 # ROOT macro to use:
 source_executable="$root_dir/PMT-ShapeAnalysis/pmt_he_study/build/xml_reader"
@@ -52,19 +52,19 @@ do
     # Prepare command in stages:
     executable_plus_arguments="$source_executable --i $file --c $config_file --o $root_file"
     #
-    interactive_command="$executable_plus_arguments >& $root_dir/batch_files/$script_log_dir/logs/batch_file_$ifile.log"
+    interactive_command="$executable_plus_arguments >& $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs/batch_file_$ifile.log"
     echo $interactive_command
     #
     # Prepare batch script in a temporary directory (not sure if this is robust)
-    touch $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    chmod a+rwx $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    echo "source $root_dir/PMT-ShapeAnalysis/pmt_he_study/setup.sh" >> $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    echo "echo 'Running batch_file_$ifile.sh ...' >> $root_dir/batch_files/$script_log_dir/logs/batch_file_$ifile.log" >> $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    echo $interactive_command >> $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
-    batch_command="qsub -q $queue $root_dir/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh"
+    touch $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    chmod a+rwx $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    echo "source $root_dir/PMT-ShapeAnalysis/pmt_he_study/setup.sh" >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    echo "echo 'Running batch_file_$ifile.sh ...' >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/logs/batch_file_$ifile.log" >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    echo $interactive_command >> $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh
+    batch_command="qsub -q $queue $root_dir/PMT-ShapeAnalysis/pmt_he_study/batch_files/$script_log_dir/scripts/batch_file_$ifile.sh"
     echo $batch_command
     # For testing (without submitting to the batch farm), comment out the next line:
-    #$batch_command
+    $batch_command
   fi
 
   echo ""
