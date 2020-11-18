ef6513ff4f8d04d12d45a2aae737cf22f28c09f2
William Quinn <william.quinn.14@ucl.ac.uk>
2020-11-18 13:25:31 +0000

    Added a merge root macro

--- calomissioning/merge.c
+++ calomissioning/merge.c
@@ -3,7 +3,7 @@
 //
 void merge()
 {
-    TChain chain("rtd_output");
+    TChain chain("T");
     for (int i=434;i<438;i++)
     {
         string file=Form("run_%d.root",i); // That should work if you have files called Sensitivity.root in subdirectories called  dir_1, dir_2 ... dir_10 Change it to match what you have!
