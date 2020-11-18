3649a87cef05309b6f4b557534c3d7f3bb50df48
William Quinn <william.quinn.14@ucl.ac.uk>
2020-11-18 13:22:06 +0000

    Added a merge root macro

--- calomissioning/merge.c
+++ calomissioning/merge.c
@@ -6,7 +6,7 @@ void merge()
     TChain chain("rtd_output");
     for (int i=434;i<438;i++)
     {
-        string file=Form("run_&d.root",i); // That should work if you have files called Sensitivity.root in subdirectories called  dir_1, dir_2 ... dir_10 Change it to match what you have!
+        string file=Form("run_%d.root",i); // That should work if you have files called Sensitivity.root in subdirectories called  dir_1, dir_2 ... dir_10 Change it to match what you have!
         chain.Add(file.c_str());
     }
     chain.Merge("output.root"); // Set this to the name you want to output
